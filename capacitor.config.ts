565ea0ae7b46d1610ff6d4a9ecee3c8a280fe207
Aleksayev1 <aleksayev@gmail.com>
2025-08-19 23:18:35 -0300

    Guia PWA Builder para Google Play

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,23 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.xzenpress.app',
   appName: 'XZenPress',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#059669",
+      showSpinner: false
+    },
+    StatusBar: {
+      style: 'DARK',
+      backgroundColor: "#059669"
+    }
+  }
 };
 
 export default config;
