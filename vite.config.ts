e14a809341fe06cf9db079f8cd019292e552d5e6
Aleksayev1 <aleksayev@gmail.com>
2025-08-19 23:37:53 -0300

    Corrigir erros do PWA Builder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: false,
+    host: true,
+    port: 5173
+  },
+  preview: {
+    https: false,
+    host: true,
+    port: 4173
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
